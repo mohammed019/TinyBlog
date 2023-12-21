b50f80e681834cc3e557d909192bb0e10150c014
Mohammed Jabbar <mohammedjabbar019@gmail.com>
2023-12-21 10:25:15 +0300

    create header and form

---
+++ src/components/FormNewPost.tsx
@@ -0,0 +1,63 @@
+"use client";
+import type { FormData } from "@/types/blogs";
+import { ChangeEvent, FormEvent, useState } from "react";
+import ReactTextareaAutosize from "react-textarea-autosize";
+const inputClass = `w-full py-2 px-3 border border-gray-300 rounded-md focus:outline-none focus-ring focus:border-blue-300`;
+
+const FormNewPost = () => {
+  const [formData, setFormData] = useState<FormData>({
+    title: "",
+    content: "",
+  });
+
+  const handleChangeForm = (
+    e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    e.preventDefault();
+
+    const { name, value } = e.target; // grab the name and the value from the input and textArea
+
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmitForm = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    console.log(formData);
+  };
+  return (
+    <form className="max-w-md mx-auto p-4" onSubmit={handleSubmitForm}>
+      <div className="mb-4">
+        <input
+          value={formData.title}
+          onChange={handleChangeForm}
+          type="text"
+          className={inputClass}
+          placeholder="Enter The Title"
+          name="title"
+        />
+      </div>
+      <div className="mb-4">
+        <ReactTextareaAutosize
+          value={formData.content}
+          onChange={handleChangeForm}
+          minRows={5}
+          name="content"
+          className={inputClass}
+          placeholder="Enter The Content"
+        />
+      </div>
+      <button
+        type="submit"
+        className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-md focus:outline-none focus:ring focus:border-blue-300 w-full disabled:bg-gray-400"
+      >
+        Create
+      </button>
+    </form>
+  );
+};
+
+export default FormNewPost;
